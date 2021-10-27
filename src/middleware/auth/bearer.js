0bc2675b6e1f010d49effb64c7ced63778777de0
Beers15 <alexanderbeers15@gmail.com>
2021-10-27 16:48:30 -0700

    Create auth api structure from starter code.

--- src/middleware/auth/bearer.js
+++ src/middleware/auth/bearer.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const { users } = require('../models')
+const { users } = require('../../models/index')
 
 module.exports = async (req, res, next) => {
-
   try {
-
     if (!req.headers.authorization) { _authError() }
 
     const token = req.headers.authorization.split(' ').pop();
@@ -13,7 +11,6 @@ module.exports = async (req, res, next) => {
     req.user = validUser;
     req.token = validUser.token;
     next();
-
   } catch (e) {
     _authError();
   }
